b2b3e5c57c215c57c4f8a428dfbc3e9e6d0de0e8
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 19:12:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -5,4 +5,15 @@ function forLoop(array) {
   return array;
 }
 
+  1) loops
+       forLoop(array)
+         adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."` to an array 25 times:
+
+      AssertionError: expected '"I am 1 strange loop."' to equal 'I am 1 strange loop.'
+      + expected - actual
+
+      -"I am 1 strange loop."
+      +I am 1 strange loop.
+
+      at Context.it (test/loops-test.js:38:35)
 
